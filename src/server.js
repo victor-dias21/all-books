26ad9bc80ed5ad5b88b2a63e46beb690bbe3b073
Victor Dias <66388678+victor-dias21@users.noreply.github.com>
2025-06-25 17:11:41 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,7 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Acesso da API disponível em http://localhost:8000")
+  console.log("Boas-vindas ao AllBooks")
+  console.log("Essa é uma API de uma loja de livros online")
+  console.log("Acesse essa API em http://localhost:8000")
 })
